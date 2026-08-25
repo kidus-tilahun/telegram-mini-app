a0b3b417102be17f8c4ae39d60091b2c6394a110
kidus-tilahun <kidustilahunwork@gmail.com>
2026-08-25 18:31:13 +0300

    feat: added orders and order_items table with basic constraints and RLS enabled

--- src/types/database.ts
+++ src/types/database.ts
@@ -67,6 +67,81 @@ export type Database = {
         }
         Relationships: []
       }
+      order_items: {
+        Row: {
+          created_at: string
+          id: string
+          order_id: string
+          price: number
+          product_id: string
+          product_name: string
+          quantity: number
+        }
+        Insert: {
+          created_at?: string
+          id?: string
+          order_id: string
+          price: number
+          product_id: string
+          product_name: string
+          quantity?: number
+        }
+        Update: {
+          created_at?: string
+          id?: string
+          order_id?: string
+          price?: number
+          product_id?: string
+          product_name?: string
+          quantity?: number
+        }
+        Relationships: [
+          {
+            foreignKeyName: "order_items_order_id_fkey"
+            columns: ["order_id"]
+            isOneToOne: false
+            referencedRelation: "orders"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "order_items_product_id_fkey"
+            columns: ["product_id"]
+            isOneToOne: false
+            referencedRelation: "products"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      orders: {
+        Row: {
+          created_at: string
+          customer_name: string
+          delivery_address: string
+          id: string
+          phone: string
+          status: string
+          total: number
+        }
+        Insert: {
+          created_at?: string
+          customer_name: string
+          delivery_address: string
+          id?: string
+          phone: string
+          status?: string
+          total?: number
+        }
+        Update: {
+          created_at?: string
+          customer_name?: string
+          delivery_address?: string
+          id?: string
+          phone?: string
+          status?: string
+          total?: number
+        }
+        Relationships: []
+      }
       products: {
         Row: {
           category_id: string
